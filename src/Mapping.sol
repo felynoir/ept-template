18c8bf5878f18b572f75611681e7642e391c34cb
felyn <felynoir@gmail.com>
2024-06-22 15:24:58 +0700

    add: mapping implementation

--- src/Mapping.sol
+++ src/Mapping.sol
@@ -1,14 +1,19 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
+
+// forge test -vv --match-path test/Mapping.t.sol
+// forge test -vv --match-test test_Mapping
 
 contract Mapping {
     mapping(string => uint256) private map;
 
-    function get(string memory key) public view returns (uint256) {
-        // TODO: add logic here
+    // returns (string memory value)
+    // returns (string memory)
+    function get(string memory key) public view returns (uint256 value) {
+        value = map[key]; // equal to -> return map[key];
     }
 
     function set(string memory key, uint256 value) public {
-        // TODO: add logic here
+        map[key] = value;
     }
 }
--- test/Mapping.t.sol
+++ test/Mapping.t.sol
@@ -1,12 +1,20 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
 import {Mapping} from "../src/Mapping.sol";
 
-contract MappingTest {
+contract MappingTest is Test {
     Mapping map;
-    function setUp() public {}
 
-    function testMapping() public {}
+    function setUp() public {
+        map = new Mapping();
+    }
+
+    function test_Mapping() public {
+        map.set("abc", 10);
+        console.log("Map get abc", map.get("abc"));
+        assertEq(map.get("abc"), 9, "abc != 10");
+        // assert(map.get("abc") == 9);
+    }
 }
