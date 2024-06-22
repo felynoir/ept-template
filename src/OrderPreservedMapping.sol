98787ab8c60b590d776f3e02f8107b0cb52636a6
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-06-22 12:40:34 +0700

    feat: add ordered map tests

--- src/OrderPreservedMapping.sol
+++ src/OrderPreservedMapping.sol
@@ -2,31 +2,39 @@
 pragma solidity ^0.8.13;
 
 library OrderPreservedMapping {
+    // a map type
     struct Map {
-        uint256 tmp;
+        // TODO: add fields here
+        bool tmp; // remove this line
     }
 
+    // get a value from the map
     function get(
         Map storage map,
         string memory key
     ) public view returns (uint256) {
-        return map.tmp;
+        // TODO: add logic here
     }
 
+    // list all values in the map ordered by insertion
     function listValues(Map memory map) public pure returns (uint256[] memory) {
-        uint256[] memory values = new uint256[](1);
-        values[0] = map.tmp;
-        return values;
+        // TODO: add logic here
     }
 
+    // set a value in the map
     function set(Map storage map, string memory key, uint256 val) public {
-        map.tmp = val;
+        // TODO: add logic here
+    }
+
+    function size(Map memory map) public pure returns (uint256) {
+        // TODO: add logic here
     }
 }
 
 contract UseOrderPreservedMapping {
     using OrderPreservedMapping for OrderPreservedMapping.Map;
     OrderPreservedMapping.Map map;
+
     function setValue(string memory key, uint256 value) public {
         map.set(key, value);
     }
--- test/OrderPreservedMapping.t.sol
+++ test/OrderPreservedMapping.t.sol
@@ -2,17 +2,50 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
-import {UseOrderPreservedMapping} from "../src/OrderPreservedMapping.sol";
+import "../src/OrderPreservedMapping.sol";
 
-contract MappingTest {
-    UseOrderPreservedMapping a;
+contract MappingTest is Test {
+    using OrderPreservedMapping for OrderPreservedMapping.Map;
 
-    function setUp() public {
-        a = new UseOrderPreservedMapping();
-    }
+    OrderPreservedMapping.Map map;
 
-    function testTmpValue() public {
-        a.setValue("", 10);
-        assert(a.getValue("") == 10);
+    function test_OrderedMap() public {
+        map.set("", 10);
+        assertEq(map.get(""), 10, "Invalid getValue ''");
+
+        map.set("a", 20);
+        assertEq(map.get("a"), 20, "Invalid getValue 'a'");
+
+        map.set("b", 30);
+        assertEq(map.get("b"), 30, "Invalid getValue 'b'");
+
+        uint256[] memory values = map.listValues();
+        assertEq(values.length, 3, "Invalid values length");
+        assertEq(values.length, map.size(), "Invalid size");
+
+        assertEq(values[0], 10, "Invalid values[0]");
+        assertEq(values[1], 20, "Invalid values[1]");
+        assertEq(values[2], 30, "Invalid values[2]");
+
+        map.set("a", 40);
+
+        uint256[] memory newValues = map.listValues();
+        assertEq(newValues.length, 3, "Invalid newValues length");
+        assertEq(newValues.length, map.size(), "Invalid size");
+
+        assertEq(newValues[0], 10, "Invalid newValues[0]");
+        assertEq(newValues[1], 40, "Invalid newValues[1]");
+        assertEq(newValues[2], 30, "Invalid newValues[2]");
+
+        map.set("aa", 2);
+
+        uint256[] memory newValues2 = map.listValues();
+        assertEq(newValues2.length, 4, "Invalid newValues2 length");
+        assertEq(newValues2.length, map.size(), "Invalid size");
+
+        assertEq(newValues2[0], 10, "Invalid newValues2[0]");
+        assertEq(newValues2[1], 40, "Invalid newValues2[1]");
+        assertEq(newValues2[2], 30, "Invalid newValues2[2]");
+        assertEq(newValues2[3], 2, "Invalid newValues2[3]");
     }
 }
